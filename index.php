c43dbae815c26d7a353463c6688a78a565e6e16c
James Kurtz <jkrrv@jkrrv.org>
2015-01-03 21:47:54 -0500

    initial

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+    <style>
+        body {
+            background-color:black;
+            color:#aaa;
+            font-family: 'Segoe UI', 'Trebuchet MS', Calibri, Tahoma, Arial, sans-serif;
+        }
+        div {
+            position:absolute;
+            top:20%;
+            width:80%;
+            left:10%;
+            background-color:#000;
+            text-align:center;
+            vertical-align:middle;
+
+        }
+        p {
+            font-size:3em;
+        }
+        span {
+            color:#666;
+        }
+        a {
+            color:#666;
+        }
+    </style>
+
+</head>
+<body>
+    <div>
+        <p>The Media Ninjas have stolen the website for some repairs.</p><p>They'll bring it back shortly.</p>
+        <span>(Though, if you're looking to pass the time, <a href="https://www.youtube.com/watch?v=ZIGe-kl6euI">check out this video about ninjas of the Japanese flavor</a>.)</span>
+    </div>
+</body>
+
+</html>
+
+
+
